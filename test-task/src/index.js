a53986d2e53cda4f55aecb891ce506f8607b5157
imnotkatya <151066476+imnotkatya@users.noreply.github.com>
2025-03-29 16:38:46 +0300

    Update index.js

--- test-task/src/index.js
+++ test-task/src/index.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let a = ''; // Первое число
     let b = ''; // Второе число
     let sign = ''; // Знак операции
-    let theme = false; // Переменная для темы (по умолчанию светлая)
+    let theme = false; // Переменная для темы
   
     const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
     const action = ['+', '-', 'X', '/'];
@@ -43,7 +43,7 @@ document.addEventListener('DOMContentLoaded', () => {
   
       // Если нажата операция
       if (action.includes(key)) {
-        sign = key === 'X' ? '*' : key; // Исправляем "X" на "*"
+        sign = key === 'X' ? '*' : key;
         out.textContent = sign;
         console.log(a, b, sign);
         return;
